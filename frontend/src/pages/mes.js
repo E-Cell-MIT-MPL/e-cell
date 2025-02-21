7e624e974a82b21a559aa3ce8fce60d530f2d83a
Ahmed S. <s.ahmed.profiquence@gmail.com>
2025-02-21 10:04:58 +0530

    Format and fix meta components accross the pages.

--- frontend/src/pages/mes.js
+++ frontend/src/pages/mes.js
@@ -5,30 +5,33 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Events from "../components/Events"
 import Speakers from "../components/Speakers";
-import VentureCapitalists from "../components/VentureCapitalists";
-import ContactInfo from "../components/ContactInfo";
+// import VentureCapitalists from "../components/VentureCapitalists";
+// import ContactInfo from "../components/ContactInfo";
 import MovingBackground from "../components/MovingBackground";
 import LightEffects from "../components/LightEffects";
 import Timeline from "../components/Timeline";
-import VideoSection from "../components/VideoSection";
+// import VideoSection from "../components/VideoSection";
 import Payment from "../components/Payment";
+import Meta from "../components/meta"
 
 const Mes = () => {
-  return (
+  return (<>
+    <Meta
+      title="MES | E-Cell"
+      description="Manipal Entrepreneurship Summit. The flagship event of MAHE organized by E-Cell MIT Manipal."
+    />
     <div className="min-h-screen bg-url('https://i.ibb.co/rdZg1Kr/k.png') text-white overflow-x-hidden">
       <MovingBackground />
       <LightEffects />
       <Navbar />
       <Hero />
- <Payment/>
+      <Payment />
       <Events />
       <Speakers />
-    <Timeline />
-
+      <Timeline />
       <Footer />
-
     </div>
-  );
+  </>);
 }
 
 export default Mes;
